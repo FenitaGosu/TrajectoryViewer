caf3d6f6e6b0f0551157422c13909caa785365f3
Evgeniy Volohovich <evgeniyvolohovich@gmail.com>
2019-06-06 22:53:48 +0700

    Добавлен класс траектории

--- Logic/Data/Trajectory.cpp
+++ Logic/Data/Trajectory.cpp
@@ -1,3 +1,64 @@
+#include <vector>
+
 #include "Trajectory.h"
 
 using namespace Logic;
+
+struct Trajectory::Impl
+{
+	struct Data
+	{
+		int depth;
+
+		double inclination;
+		double azimuth;
+
+		double x;
+		double y;
+		double z;
+	};
+
+	std::vector<Data> data;
+};
+
+Trajectory::Trajectory()
+	: m_impl(std::make_unique<Impl>())
+{
+}
+
+Trajectory::~Trajectory() = default;
+
+size_t Trajectory::Size() const noexcept
+{
+	return m_impl->data.size();
+}
+
+int Trajectory::GetDepth(size_t index) const
+{
+	return m_impl->data[index].depth;
+}
+
+double Trajectory::GetInclination(size_t index) const
+{
+	return m_impl->data[index].inclination;
+}
+
+double Trajectory::GetAzimuth(size_t index) const
+{
+	return m_impl->data[index].azimuth;
+}
+
+double Trajectory::GetX(size_t index) const
+{
+	return m_impl->data[index].x;
+}
+
+double Trajectory::GetY(size_t index) const
+{
+	return m_impl->data[index].y;
+}
+
+double Trajectory::GetZ(size_t index) const
+{
+	return m_impl->data[index].z;
+}
--- Logic/Data/Trajectory.h
+++ Logic/Data/Trajectory.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <memory>
+
 namespace Logic
 {
 	class Trajectory
 	{
+	public:
+		Trajectory();
+		~Trajectory();
+
+		size_t Size() const noexcept;
+
+		int		GetDepth(size_t index) const;
+		double	GetInclination(size_t index) const;
+		double	GetAzimuth(size_t index) const;
+
+		double GetX(size_t index) const;
+		double GetY(size_t index) const;
+		double GetZ(size_t index) const;
+
+	private:
+		struct Impl;
+		std::unique_ptr<Impl> m_impl;
 	};
 }
